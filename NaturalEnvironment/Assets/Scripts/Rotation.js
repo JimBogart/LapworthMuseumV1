5da1e83a260cd42321ae2c6eeaaa878f72038ade
Jim Bogart <jimilives484@gmail.com>
2017-06-30 13:18:15 +0100

    Initial files
    Initial file commit

---
+++ NaturalEnvironment/Assets/Scripts/Rotation.js
@@ -0,0 +1,69 @@
+﻿#pragma strict
+
+//Camera Transform
+var Cam : Transform;
+//Difference variable to reset the user's view based on their current gyroscopic readings
+var difference : Quaternion;
+//The variable for the gyroscope's readings
+var gyroscopeValues : Quaternion;
+
+function Start () {
+//Enables the gyroscope, essentially turning it on
+Input.gyro.enabled = true;
+
+//Sets the beggining difference to 0, this will lock the view when the app is started. Just tap the screen to start looking around
+difference = Quaternion(0,0,0,0);
+}
+
+
+
+function Update () {
+//Aquires the gyroscope's values
+gyroscopeValues = Input.gyro.attitude;
+//Aquires the difference between the current gyroscopic readings and the user's set difference values. This allows the user to reset the view.
+Cam.rotation = Quaternion.Inverse(gyroscopeValues)*difference;
+//Flips the camera's z axis
+Cam.rotation.z *=-0.01;
+}
+
+
+
+function Touch(){
+//This function is called every time a person touches anywhere on the screen
+
+
+//Resets the difference variable to the current gyroscopic values
+difference = Input.gyro.attitude;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//I know that the BB8 textures are messed up
+
+
+
+
+
---
+++ NaturalEnvironment/Assets/Scripts/Walk.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walk : MonoBehaviour {
+
+    public GameObject model;
+    public float walkSpeed;
+   
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        model.transform.position += model.transform.TransformDirection(Vector3.back) * walkSpeed;
+
+    }
+}
